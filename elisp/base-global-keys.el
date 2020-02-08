b0d08959a4567e3cdc366ca1b7934066d62ebe3f
Frank de Jong <f.jin@protonmail.com>
2020-02-08 14:48:37 +0100

    Add half-scroll keybinds

--- elisp/base-global-keys.el
+++ elisp/base-global-keys.el
@@ -2,10 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
+(autoload 'View-scroll-half-page-forward "view")
+(autoload 'View-scroll-half-page-backward "view")
 
 ;; (global-set-key (kbd "[SHORTCUT]") '[FUNCTION])
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-c o") 'browse-url)
+(global-set-key (kbd "C-v") 'View-scroll-half-page-forward)
+(global-set-key (kbd "M-v") 'View-scroll-half-page-backward)
 
 
 (provide 'base-global-keys)
